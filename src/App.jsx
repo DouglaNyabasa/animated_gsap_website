f475efa7cb28bc190cbe3c6c888875f9d905b9d4
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-07 16:44:24 +0200

    message section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all';
+import Message from './components/Message/Message';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
-    <div>
+    <main>
     <Navbar/>
     <Hero/>
-    </div>
+    <Message/>
+    <div className="h-dvh border border-amber-500"></div>
+    </main>
   )
 }
 
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -45,6 +45,20 @@ const Hero = () => {
         "-=0.5"
       );
 
+    const heroTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+    heroTl.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    })
   });
 
   return (
