94a6fec42568f4179f56770e56d389664b6af336
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-09 12:34:23 +0200

    flavor slider updated its responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all';
-import Message from './components/Message/Message';
-import Flavors from './components/Flavors/Flavors';
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import gsap from "gsap";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import Message from "./components/Message/Message";
+import Flavors from "./components/Flavors/Flavors";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
   return (
-    <main>
-    <Navbar/>
-    <Hero/>
-    <Message/>
-    <Flavors/>
-    <div className="h-dvh border border-amber-500"></div>
-    </main>
-  )
-}
+      <main>
+      <Navbar />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Hero />
+          <Message />
+          <Flavors />
+         <div className="h-dvh border border-amber-500"></div>
 
-export default App
+
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -8,13 +8,14 @@ const Message = () => {
     const firstMsgSplit = SplitText.create(".first-message", {
       type: "words",
     });
-    const secondMsgSplit = SplitText.create(".second-message", {
+    const secMsgSplit = SplitText.create(".second-message", {
       type: "words",
     });
     const paragraphSplit = SplitText.create(".message-content p", {
       type: "words, lines",
       linesClass: "paragraph-line",
     });
+
     gsap.to(firstMsgSplit.words, {
       color: "#faeade",
       ease: "power1.in",
@@ -26,7 +27,7 @@ const Message = () => {
         scrub: true,
       },
     });
-      gsap.to(secondMsgSplit.words, {
+    gsap.to(secMsgSplit.words, {
       color: "#faeade",
       ease: "power1.in",
       stagger: 1,
@@ -37,35 +38,33 @@ const Message = () => {
         scrub: true,
       },
     });
+
     const revealTl = gsap.timeline({
-        delay: 1,
-        scrollTrigger:{
-            trigger:".msg-text-scroll",
-            start:"top 60%",
-        }
+      delay: 1,
+      scrollTrigger: {
+        trigger: ".msg-text-scroll",
+        start: "top 60%",
+      },
     });
-    revealTl.to(".msg-text-scroll",
-      {
-        duration:1,
-         clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-         ease:"circ.inOut",
-      }
-    );
+    revealTl.to(".msg-text-scroll", {
+      duration: 1,
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      ease: "circ.inOut",
+    });
+
     const paragraphTl = gsap.timeline({
-        scrollTrigger:{
-            trigger:".message-content p",
-            start: "top center",
-        }
+      scrollTrigger: {
+        trigger: ".message-content p",
+        start: "top center",
+      },
     });
-     paragraphTl.from(paragraphSplit.words,
-      {
-        yPercent: 300,
-        rotate: 3,
-        ease: "power1.inOut",
-        duration:1,
-        stagger: 0.01
-      }
-    );
+    paragraphTl.from(paragraphSplit.words, {
+      yPercent: 300,
+      rotate: 3,
+      ease: "power1.inOut",
+      duration: 1,
+      stagger: 0.01,
+    });
   });
 
   return (
@@ -73,20 +72,24 @@ const Message = () => {
       <div className="container mx-auto flex-center py-28 relative">
         <div className="w-full h-full">
           <div className="msg-wrapper">
-            <h1 className="first-message">STIR UP YOUR FEARLESS PAST AND</h1>
+            <h1 className="first-message">Stir up your fearless past and</h1>
+
             <div
-             style={{
-                clipPath:"polygon(0 0, 0 0, 0 100%, 0% 100%)"
-             }}
-             className="msg-text-scroll">
+              style={{
+                clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+              }}
+              className="msg-text-scroll"
+            >
               <div className="bg-light-brown md:pb-5 pb-3 px-5">
-                <h2 className="text-red-brown">FUEL UP</h2>
+                <h2 className="text-red-brown">Fuel Up</h2>
               </div>
             </div>
+
             <h1 className="second-message">
               your future with every gulp of Perfect Protein
             </h1>
           </div>
+
           <div className="flex-center md:mt-20 mt-10">
             <div className="max-w-md px-10 flex-center overflow-hidden">
               <p>
