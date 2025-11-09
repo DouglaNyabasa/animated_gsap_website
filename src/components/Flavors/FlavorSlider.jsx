f59faad6d5c607fa494fa99f6e7058e87584a7ba
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-09 11:33:05 +0200

    flavor slider

--- src/components/Flavors/FlavorSlider.jsx
+++ src/components/Flavors/FlavorSlider.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { flavorlists } from "../../constants";
 
 const FlavorSlider = () => {
   return (
-    <div>
-      
+    <div className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+            <div
+              className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${flavor.rotation}`}
+              key={flavor.name}
+            >
+              <img src={` /images/${flavor.color}-bg.svg`} alt="" className="absolute bottom-0"/>
+              <img src={`/images/${flavor.color}-drink.webp`} alt="" className="drinks"/>
+              <img src={`/images/${flavor.color}-elements.webp`} alt="" className="elements"/>
+              <h1>{flavor.name}</h1>
+            </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FlavorSlider
+export default FlavorSlider;
