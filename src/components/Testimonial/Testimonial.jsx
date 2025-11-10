4353c9626a6f2a2aaccff07e22cbf564bb4c5798
douglanyabasa <douglasnyabasa400@gmail.com>
2025-11-10 14:36:05 +0200

    updated the animations of the testimonials section

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -17,6 +17,43 @@ const Testimonial = () => {
     const video = vdRef.current[index];
     video.pause();
   };
+
+  useGSAP(() => {
+    gsap.set(".testimonials-section", {
+      marginTop: "-140vh",
+    });
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".testimonials-section",
+        start: "top bottom",
+        end: "200% top",
+        scrub: true,
+      },
+    });
+    tl.to(
+      ".testimonials-section .first-title",
+      {
+        xPercent: 70,
+      },
+      "<"
+    )
+      .to(
+        ".testimonials-section .sec-title",
+        {
+          xPercent: 25,
+        },
+        "<"
+      )
+      .to(
+        ".testimonials-section .third-title",
+        {
+          xPercent: -50,
+        },
+        "<"
+      );
+
+      const pinTl = gsap
+  });
   return (
     <section className="testimonials-section">
       <div className="absolute size-full flex flex-col items-center pt-[5vw]">
